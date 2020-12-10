ed68733f28751b9137cbc0eb3f9576969b35f188
ramesh <rsoft.ramesh@gmail.com>
2020-12-10 17:34:17 +0530

    middleware

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,8 +3,13 @@ const express = require('express');
 const cors = require('cors');
 import {Request, Response} from 'express';
 
+const validateToken = async (req: Request, res: Response, next: Function) => {
+  console.log('Check if request is authorized with Firebase ID token');
+}
+
 const app = express();
 app.use(cors({ origin: true }));
+app.user(validateToken);
 
 app.post('/', (req: Request, res: Response) => res.send("Hello World\n"));
 
